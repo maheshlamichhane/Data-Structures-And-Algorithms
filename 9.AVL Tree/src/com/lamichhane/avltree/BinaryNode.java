bd9a488e8d1b00fac463a3b00873dfcc3d62ef73
unknown <softwarengineer007@gmail.com>
2024-01-30 22:07:36 +0545

    AVL Tree Completed

---
+++ 9.AVL Tree/src/com/lamichhane/avltree/BinaryNode.java
@@ -0,0 +1,14 @@
+package com.lamichhane.avltree;
+
+public class BinaryNode {
+
+	public int value;
+	public int height;
+	public BinaryNode left;
+	public BinaryNode right;
+	
+	BinaryNode(){
+		this.height = 0;
+	}
+	
+}
---
+++ 9.AVL Tree/src/com/lamichhane/avltree/Main.java
@@ -0,0 +1,18 @@
+package com.lamichhane.avltree;
+
+public class Main {
+
+	public static void main(String args[]) {
+		
+		AVL avl = new AVL();
+		avl.insert(5);
+		avl.insert(10);
+		avl.insert(15);
+		avl.insert(20);
+		
+		avl.levelOrder();
+		System.out.println();
+		avl.delete(5);
+		avl.levelOrder();
+	}
+}
